52aa3666a4dad60166daa1196882195fa21698ed
dont-wait <nguyentansang15042005@gmail.com>
2025-12-31 18:09:19 +0700

    feat: initialize entities for module

---
+++ server/src/main/java/com/minori/server/entity/Role.java
@@ -0,0 +1,34 @@
+package com.minori.server.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "roles")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    Integer roleId;
+
+    @Column(name = "role_name", length = 50, nullable = false, unique = true)
+    String roleName;
+
+    @Column(name = "role_description", columnDefinition = "TEXT")
+    String roleDescription;
+
+    @OneToMany(mappedBy = "role",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY)
+    List<RolePermission> rolePermissions;
+}
