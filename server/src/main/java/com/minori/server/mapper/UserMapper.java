ec4e984d3dbb8a948c5113efc2ed65b23467b39e
dont-wait <nguyentansang15042005@gmail.com>
2026-01-05 08:56:01 +0700

    fix: handle Role in method toUser

--- server/src/main/java/com/minori/server/mapper/UserMapper.java
+++ server/src/main/java/com/minori/server/mapper/UserMapper.java
@@ -2,8 +2,10 @@ package com.minori.server.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import com.minori.server.dto.request.auth.UserCreationRequest;
+import com.minori.server.dto.request.auth.UserUpdateRequest;
 import com.minori.server.dto.response.auth.UserResponse;
 import com.minori.server.entity.Role;
 import com.minori.server.entity.User;
@@ -12,6 +14,9 @@ import com.minori.server.entity.User;
 public interface UserMapper {
 
     @Mapping(target = "roleName", source="role.roleName")
-    UserResponse toUserResponse(User user, Role role);
-    User toUser(UserCreationRequest request);
+    UserResponse toUserResponse(User user);
+
+    User toUser(UserCreationRequest request, Role role);
+    
+    void updateUser(UserUpdateRequest request, @MappingTarget User user);
 }
