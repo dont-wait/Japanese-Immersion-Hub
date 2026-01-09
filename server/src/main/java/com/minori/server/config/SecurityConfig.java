4af9469a5b8d3cfe6b11c9ee855738b32dc08c79
dont-wait <nguyentansang15042005@gmail.com>
2026-01-09 17:24:31 +0700

    feat: add OAuth2 resource server dependency and configure security filter chain

--- server/src/main/java/com/minori/server/config/SecurityConfig.java
+++ server/src/main/java/com/minori/server/config/SecurityConfig.java
@@ -2,11 +2,36 @@ package com.minori.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private static final String[] PUBLIC_ENDPOINTS = {
+        "/auth/**",
+        "/users/register",
+        "/users/login",
+        "/swagger-ui/**",
+        "/v3/api-docs/**"                
+    };
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+            .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(request -> request
+                    .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+                .anyRequest().authenticated()
+            );
+        return httpSecurity.build();
+    }
+
     @Bean
     //Khong can new nhieu lan
     PasswordEncoder passwordEncoder() {
