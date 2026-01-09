5e68ced0fcad87bde4b1e5b08dfcef96062a8879
dont-wait <nguyentansang15042005@gmail.com>
2026-01-09 18:03:50 +0700

    feat(security): enhance security by create jwt and decode it

--- server/src/main/java/com/minori/server/config/SecurityConfig.java
+++ server/src/main/java/com/minori/server/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.minori.server.config;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,11 +10,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${jwt.signerKey}")
+    private String signerKey;
 
     private static final String[] PUBLIC_ENDPOINTS = {
         "/auth/**",
@@ -24,12 +33,23 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-            .csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(request -> request
-                    .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
-                .anyRequest().authenticated()
-            );
+                        .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(
+                        oauth2 -> oauth2.jwt(jwtConfig -> jwtConfig.decoder(jwtDecoder())));
+
         return httpSecurity.build();
+    }
+    
+    @Bean
+    JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKey = new SecretKeySpec(signerKey.getBytes(), "HS512");
+        return NimbusJwtDecoder
+                .withSecretKey(secretKey)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
     }
 
     @Bean
--- server/src/main/java/com/minori/server/controller/AuthenticationController.java
+++ server/src/main/java/com/minori/server/controller/AuthenticationController.java
@@ -2,6 +2,8 @@ package com.minori.server.controller;
 
 import java.text.ParseException;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,18 +30,23 @@ public class AuthenticationController {
     AuthenticationService authenticationService;
 
     @PostMapping("/login")
-    public ApiResponse<AuthenticationResponse> login(@Valid @RequestBody AuthenticationRequest request) {
+    public ResponseEntity<ApiResponse<AuthenticationResponse>> login(@Valid @RequestBody AuthenticationRequest request) {
         var authenticationResponse = authenticationService.authenticate(request);
-        return ApiResponse.<AuthenticationResponse>builder()
+        return ResponseEntity
+        .status(HttpStatus.CREATED)
+        .body(ApiResponse.<AuthenticationResponse>builder()
+                .message("User logged in successfully")
                 .result(authenticationResponse)
-                .build();
+                .build());
     }
 
     @PostMapping("/introspect")
-    public ApiResponse<IntrospectResponse> login(@RequestBody IntrospectRequest request) throws JOSEException, ParseException {
+    public ResponseEntity<ApiResponse<IntrospectResponse>> introspect(@RequestBody IntrospectRequest request) throws JOSEException, ParseException {
         var authenticationResponse = authenticationService.introspect(request);
-        return ApiResponse.<IntrospectResponse>builder()
-                .result(authenticationResponse)
-                .build();
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(ApiResponse.<IntrospectResponse>builder()
+                    .result(authenticationResponse)
+                    .build());
     } 
 }
