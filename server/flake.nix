7536d3dcd2ee4249eceec8d306094c93c90d887e
dont-wait <nguyentansang15042005@gmail.com>
2026-05-01 20:11:09 +0700

    feat: update flake.nix

--- server/flake.nix
+++ server/flake.nix
@@ -6,26 +6,61 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
+          overlays = [
+            (final: prev: {
+              jdk = prev.jdk25;
+              maven = prev.maven.override { jdk_headless = prev.jdk25; };
+              lombok = prev.lombok.override { jdk = prev.jdk25; };
+            })
+          ];
         };
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
+            gcc
+            ncurses
+            patchelf
+            zlib
+            tree
             jdk25
             maven
-            liquibase 
+            gradle
+            liquibase
             postgresql_jdbc
+            jdt-language-server
+            lombok
           ];
 
           shellHook = ''
             export JAVA_HOME=${pkgs.jdk25.home}
-            
+            export PATH="${pkgs.jdk25}/bin:$PATH"
+            export JDTLS_HOME=${pkgs.jdt-language-server}/share/java/jdtls
+
+            mkdir -p ~/.local/share/nvim/mason/packages/jdtls
+            rm -rf ~/.local/share/nvim/mason/packages/jdtls/plugins
+            rm -f ~/.local/share/nvim/mason/packages/jdtls/lombok.jar
+
+            ln -sf ${pkgs.jdt-language-server}/share/java/jdtls/plugins ~/.local/share/nvim/mason/packages/jdtls/plugins
+            ln -sf ${pkgs.lombok}/share/java/lombok.jar ~/.local/share/nvim/mason/packages/jdtls/lombok.jar
+
+            if [ ! -d ~/.local/share/nvim/mason/packages/jdtls/config_linux ]; then
+              cp -r ${pkgs.jdt-language-server}/share/java/jdtls/config_linux ~/.local/share/nvim/mason/packages/jdtls/config_linux
+              chmod -R u+w ~/.local/share/nvim/mason/packages/jdtls/config_linux
+            fi
+
             echo "------------------------------------------"
             echo "🚀 Java 25, Maven 3.9 & Liquibase Shell Active"
             echo "JDK Path: $JAVA_HOME"
@@ -41,10 +76,14 @@
           build = pkgs.stdenv.mkDerivation {
             name = "java-project-test";
             src = ./.;
-            buildInputs = [ pkgs.jdk25 pkgs.maven ];
+            buildInputs = [
+              pkgs.jdk25
+              pkgs.maven
+            ];
             buildPhase = "mvn test";
             installPhase = "touch $out";
           };
         };
-      });
+      }
+    );
 }
