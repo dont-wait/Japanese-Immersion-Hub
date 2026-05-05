fa6a4132d2690e7369dbb2eae223b23c95ed0e27
dont-wait <nguyentansang15042005@gmail.com>
2026-05-05 22:07:04 +0700

    feat: add cicd pineline for code quality

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier', // tắt rule conflict với prettier, để cuối cùng
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': 'warn',
+  },
+}
